8d3d0d6fe742a0da017a3503e51fd8e6fc722a75
Jamean <147342221+Jamean-Groenewald@users.noreply.github.com>
2024-08-14 22:06:33 +0200

    Update decrypt_and_print.asm

--- decrypt_and_print.asm
+++ decrypt_and_print.asm
@@ -9,8 +9,10 @@ section .data
     ; Do not modify anything above this line unless you know what you are doing
     ; ==========================
     ; Your data goes here
+    ; prompt db 'Enter ciphertext to decrypt: ', 0
+	; prompt_length equ $ - prompt
 
-    xor_key dq 0x73113777
+    xor_key db 0x73113777
 
     ; ==========================
 
@@ -32,25 +34,32 @@ decrypt_and_print:
     ; ==========================
     ; Your code goes here
 
+    ; mov rax, 1 			; Syscall number for syswrite
+	; mov rdi, 1			; Specify file descriptor,1 for stdout
+	; mov rsi, prompt
+	; mov rdx, prompt_length
+	; syscall
+
     mov rax, rdi
     xor rax, [xor_key]
     ror rax, 4
     call print_char_32
 
-    mov rax, rdi
+    mov rax, rsi
     xor rax, [xor_key]
     ror rax, 4
     call print_char_32
     
-    mov rax, rdi
+    mov rax, rdx
     xor rax, [xor_key]
     ror rax, 4
     call print_char_32
 
-    mov rax, rdi
+    mov rax, rcx
     xor rax, [xor_key]
     ror rax, 4
     call print_char_32
+
     ; ==========================
     ; Do not modify anything below this line unless you know what you are doing
 
